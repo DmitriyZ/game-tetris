67d44ca479f35cd151fdb4964551e014e50cd6e9
Заец Дмитрий Иванович <DIZaets@sberbank.ru>
2021-03-14 00:33:32 +0300

    Initial commit

---
+++ src/main/java/ru/zaets/home/game/tetris/Main.java
@@ -0,0 +1,12 @@
+package ru.zaets.home.game.tetris;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JWindow window = new JWindow();
+        window.setVisible(true);
+        window.setSize(200, 400);
+        window.setLocation(100, 100);
+    }
+}
